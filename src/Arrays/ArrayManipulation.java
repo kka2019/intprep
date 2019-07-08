94541cdf3cce0ab9fc4df0944f426e75e87b89bb
Kishan Kushwaha <kkprog18@gmail.com>
2019-07-08 22:27:39 +0530

    ArrayManipulationCompleted

--- src/Arrays/ArrayManipulation.java
+++ src/Arrays/ArrayManipulation.java
@@ -8,32 +8,49 @@ public class ArrayManipulation {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        int[] arr = new int[n];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = 0;
-        }
-
-        int[][] queries = new int[m][3];
+        long[][] queries = new long[m][3];
 
         for (int i = 0; i < m; i++) {
-            queries[i][0] = scanner.nextInt();
-            queries[i][1] = scanner.nextInt();
-            queries[i][2] = scanner.nextInt();
+            queries[i][0] = scanner.nextLong();
+            queries[i][1] = scanner.nextLong();
+            queries[i][2] = scanner.nextLong();
         }
+
+        System.out.println(arrayManipulation(n,queries));
     }
 
-    public static int arrayManipulation(int arr, int[][] queries){
-        int maxValue = 0;
+    public static long arrayManipulation(int n, long[][] queries){
 
+        long[] arr = new long[n+1];
         for(int i=0; i<queries.length; i++){
-            int a = queries[i][0];
-            int b = queries[i][1];
-            int k = queries[i][2];
+            long a = queries[i][0] - 1;
+            long b = queries[i][1];
+            long k = queries[i][2];
 
-            for(int j=a; j<=b; j++){
-                arr[j] += k;
-            }
+//            for(int j=(int)a; j<=(int)b; j++){
+//                arr[j] += k;
+//            }
+
+            arr[(int)a] +=  k;
+            arr[(int)b] -= k;
         }
-        return maxValue;
+
+        long sum = 0;
+        long max = 0;
+        for(int i=0; i<n+1; i++){
+            sum += arr[i];
+            max = Math.max(max,sum);
+        }
+        return max;
     }
+
+//    public static long maxArrayValue(long[] arr){
+//        long max = 0;
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i] > max){
+//                max = arr[i];
+//            }
+//        }
+//        return max;
+//    }
 }
